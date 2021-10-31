0f2cb28f94b7d54bc9e92a9109ba202e1ba60feb
MSierag <msierag@gmail.com>
2021-10-31 10:59:33 +0000

    Continue deployment

--- spincycle/settings.py
+++ spincycle/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1#9o0=-htydx9hf6#^&mw644xs*jtvgvnx_i8*#--erq8xr#uu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['spincycle-records.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,7 +119,12 @@ WSGI_APPLICATION = 'spincycle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
