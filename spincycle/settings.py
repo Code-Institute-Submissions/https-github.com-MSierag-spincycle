a75469a4d480a5eb5967d82c56c046aa44bad711
MSierag <msierag@gmail.com>
2021-10-31 11:15:06 +0000

    Set up Heroku deployment

--- spincycle/settings.py
+++ spincycle/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1#9o0=-htydx9hf6#^&mw644xs*jtvgvnx_i8*#--erq8xr#uu'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['spincycle-records.herokuapp.com', 'localhost']
 
